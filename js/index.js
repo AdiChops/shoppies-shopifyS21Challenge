207be510d364883f42ab2d9143bb885a879227bc
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-01-05 15:44:11 -0500

    some styling and nomination basic functionality

--- js/index.js
+++ js/index.js
@@ -1,30 +1,52 @@
 let $$ = (id) => { return document.getElementById(id);};
 
-console.log($$("searchTitle"));
-$$("searchTitle").addEventListener('change', ()=>{
+$$("searchTitle").addEventListener('keyup', ()=>{
     let searchText = $$("searchTitle").value;
     if(searchText == ""){
+        $$("pageSelection").style.display = "none";
         $$("resultsHeader").textContent = "";
-        $$("results").innerHtml = "";
+        $$("results").innerHTML = "";
     }
     else{
         $$("resultsHeader").textContent = `Results for "${searchText}"`;
         // perform API call
-        fetch(`https://omdbapi.com/?apikey=bf80773c&s=${searchText}`).then((resp) => {
-            return resp.json();
-        }).then((info)=>{
-            console.log(info);
-            if(info["Response"] == "False"){
-                $$("results").textContent = "That search returned no results.";
-            } // if invalid call
-            else{
-                let films = info["Search"];
-                let results = "<ul>";
-                for(let film in films){
-                    results += `<li>${film["Title"]} (${film["Year"]})`;
-                }
-                $$("results").innerHtml = results;
-            }
-        });
+        let numResults = performSearch(searchText, 1);
+        console.log(numResults);
+        let pagesOptions = new Array(Math.ceil(numResults/10)+1);
+        pagesOptions.pop(); // getting rid of page 0
+        $$("pages").options = pagesOptions;
+        $$("pageSelestion").style.display = "block";
     }
-})
+});
+
+let performSearch = (searchText, p) => {
+    let num = 0;
+    fetch(`https://omdbapi.com/?apikey=bf80773c&s=${searchText}&page=${p}`).then((resp) => {
+        return resp.json();
+    }).then((info)=>{
+        if(info["Response"] == "False"){
+            $$("results").textContent = info["Error"];
+            $$("pages").style.display = "none";
+        } // if invalid call
+        else{
+            let films = info["Search"];
+            let results = "<ul>";
+            for(let i in films){
+                let film = films[i];
+                results += `<li>${film["Title"]} (${film["Year"]}) <button name="nominate" value="${i}" id="${film["imdbID"]}"}>Nominate</button></li>`;
+            }
+            results+="</ul>"
+            $$("results").innerHTML = results;
+            num = parseInt(info["totalResults"]);
+            let nominates = document.getElementsByName("nominate");
+            for(let n of nominates){
+                n.addEventListener('click', ()=>{
+                    let val = films[n.value];
+                    $$("nominations").innerHTML += `<p>${val["Title"]} (${val["Year"]})</p>`;
+                    console.log(val);
+                });
+            }
+        }
+    });
+    return num;
+};
