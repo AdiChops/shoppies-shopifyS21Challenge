e54978b913630d118486d2ba4bbcfd2c33f49246
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-01-08 23:17:13 -0500

    comments

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 let $$ = (id) => { return document.getElementById(id);};
 const MAX_NOMINEES = 5;
 
+// populating the nominees in localStorage once the page loads
 addEventListener('load', ()=>{
     populateNominees();
 });
 
-$$("searchTitle").addEventListener('keyup', ()=>{
+$$("searchTitle").addEventListener('keyup', async ()=>{
     let searchText = $$("searchTitle").value;
     if(searchText == ""){
         $$("pageSelection").style.display = "none";
@@ -16,19 +17,24 @@ $$("searchTitle").addEventListener('keyup', ()=>{
     else{
         $$("resultsHeader").textContent = `Results for "${searchText}"`;
         // perform API call
-        let numResults = performSearch(searchText, 1);
-        console.log(numResults);
+        let numResults = await performSearch(searchText, 1);
         let pagesOptions = new Array(Math.ceil(numResults/10)+1);
         pagesOptions.pop(); // getting rid of page 0
         $$("pages").options = pagesOptions;
-        // $$("pageSelestion").style.display = "block";
+        $$("pages").style.display = "block";
         $$("resultsDiv").style.display = "block";
     }
 });
 
-let performSearch = (searchText, p) => {
+/**
+ * This function performs the search for the user.
+ * @param {*} searchText represents what the user types into the search field
+ * @param {*} p represents the page of results that the user would like to view
+ */
+let performSearch = async (searchText, p) => {
     let num = 0;
-    fetch(`https://omdbapi.com/?apikey=bf80773c&s=${searchText}&page=${p}`).then((resp) => {
+    // calling the API
+    await fetch(`https://omdbapi.com/?apikey=bf80773c&s=${searchText}&page=${p}`).then((resp) => {
         return resp.json();
     }).then((info)=>{
         if(info["Response"] == "False"){
@@ -59,13 +65,22 @@ let performSearch = (searchText, p) => {
             }
         }
     });
+    
     return num;
 };
 
+/**
+ * This functions returns whether a "nominate" button should be disabled, depending on if the movie is already nominated,
+ * or if the maximum number of nominations is reached.
+ * @param {*} movieId represents the imdbID of the movie, which represents the "nominate" button for that movie
+ */
 let disableButton = (movieId) =>{
     return (localStorage.getItem(movieId) != null || nominationsIsFull())? "disabled":"";
 };
 
+/**
+ * This function disables all of the "nominate" buttons
+ */
 let disableAll = () =>{
     let buttons = document.getElementsByName("nominate");
     for(let button of buttons){
@@ -73,10 +88,17 @@ let disableAll = () =>{
     }
 };
 
+/**
+ * This function determines if the maximum number of nominations is reached.
+ */
 let nominationsIsFull = () =>{
     return Object.keys(localStorage).length >= MAX_NOMINEES;
 };
 
+/**
+ * This function is called when the page is first loaded. It populates the nominations div with all
+ * of the nominations that are still stored in localStorage.
+ */
 let populateNominees = () => {
     let keys = Object.keys(localStorage);
     if(keys.length != 0){
@@ -87,16 +109,25 @@ let populateNominees = () => {
     }
 };
 
+/**
+ * This function nominates a movie (adding it to local storage and to the nominations div) 
+ * and displays the modal/badge if the max numbe of nominations is reached
+ * @param {*} item represents the movie getting nominated
+ */
 let addNominee = (item) => {
     $$("nominations").insertAdjacentHTML( 'beforeend', `<p id="nom${item["imdbID"]}">${item["Title"]} (${item["Year"]}) <button class="btn btn-outline-danger btn-sm" title="Remove nomination" id="rem${item["imdbID"]}"><i class="fas fa-times"></i></button></p>`);
     $$(`rem${item["imdbID"]}`).addEventListener('click', () => {removeNominee(item["imdbID"])});
+    // Checking if more nominations can be displayed
     if(nominationsIsFull()){
-        $("#nomFullModal").modal();
-        disableAll();
-        $$("maxNoms").textContent = `${MAX_NOMINEES} nominations reached!`;
+        $("#nomFullModal").modal(); //displaying modal
+        disableAll(); // disabling all "nominate" buttons
+        $$("maxNoms").textContent = `${MAX_NOMINEES} nominations reached!`; // displaying badge
     }
 };
 
+/**
+ * This function enables all of the "nominate" buttons (except the movies already nominated)
+ */
 let reEnableAll = () =>{
     let buttons = document.getElementsByName("nominate");
     for(let button of buttons){
@@ -106,6 +137,10 @@ let reEnableAll = () =>{
     }
 };
 
+/**
+ * This functions removes a nominations from the nominee list (from the div and from localStorage)
+ * @param {*} id represents the imdbID of the movie getting removed from nominations
+ */
 let removeNominee = (id) => {
     localStorage.removeItem(id);
     let nominateBtn = $$(id);
@@ -114,6 +149,7 @@ let removeNominee = (id) => {
     if(nominateBtn != undefined){
         nominateBtn.disabled = false;
     }
+    // since the number of nominations is not at the maximum anymore, reEnableAll() is called
     reEnableAll();
     let keys = Object.keys(localStorage);
     if(keys.length == 0){
